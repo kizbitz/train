97f154262d473d9c07394847e75d14fc86756f06
Aaron Huslage <huslage@gmail.com>
2015-09-21 21:42:18 -0400

    Add all supported AMIs
    Removed unsupported OS versions and added AMIs from Marketplace for all regions.

--- train/vpc/amis.py
+++ train/vpc/amis.py
@@ -4,15 +4,45 @@
 """Dictionaries for AWS AMI's per region"""
 
 EU_CENTRAL_1 = {
+    'AMAZON-LINUX': 'ami-a6b0b7bb',
+    'CENTOS-7.0': 'ami-7cc4f661',
+    'DEBIAN-JESSIE': 'ami-b092aaad',
+    'DEBIAN-WHEEZY': 'ami-98043785',
+    'SUSE-SLES-12': 'ami-a22610bf',
+    'Ubuntu-14.04': 'ami-4ec4f653',
 }
 
 SA_EAST_1 = {
+    'AMAZON-LINUX': 'ami-55098148',
+    'CENTOS-7.0': 'ami-bf9520a2',
+    'DEBIAN-JESSIE': 'ami-a5bd3fb8',
+    'DEBIAN-WHEEZY': 'ami-3d8b3720',
+    'RHEL-7.0': 'ami-595ce844',
+    'RHEL-7.1': 'ami-09e25b14',
+    'SUSE-SLES-12': 'ami-f102b6ec',
+    'Ubuntu-14.04': 'ami-55f14e48',
 }
 
 AP_NORTHEAST_1 = {
+    'AMAZON-LINUX': 'ami-1c1b9f1c',
+    'CENTOS-7.0': 'ami-89634988',
+    'DEBIAN-JESSIE': 'ami-fe54f3fe',
+    'DEBIAN-WHEEZY ': 'ami-b25d44b3',
+    'RHEL-7.0': 'ami-35556534',
+    'RHEL-7.1': 'ami-b1b458b1',
+    'SUSE-SLES-12': 'ami-d54a79d4',
+    'Ubuntu-14.04': 'ami-5abca75b',
 }
 
 EU_WEST_1 = {
+    'AMAZON-LINUX': 'ami-e4d18e93',
+    'CENTOS-7.0': 'ami-e4ff5c93',
+    'DEBIAN-JESSIE': 'ami-0ed89d79',
+    'DEBIAN-WHEEZY': 'ami-61e56916',
+    'RHEL-7.0': 'ami-8cff51fb',
+    'RHEL-7.1': 'ami-25158352',
+    'SUSE-SLES-12': 'ami-e801af9f',
+    'Ubuntu-14.04': 'ami-f5810f82',
 }
 
 US_EAST_1 = {
@@ -20,24 +50,55 @@ US_EAST_1 = {
     'CENTOS-7.0': 'ami-96a818fe',
     'DEBIAN-JESSIE': 'ami-144f4d7c',
     'DEBIAN-WHEEZY': 'ami-e0efab88',
-    'RHEL-6.5': 'ami-00a11e68',
-    'RHEL-6.6': 'ami-48400720',
-    'RHEL-7.0': 'ami-785bae10',
+    'RHEL-7.0': 'ami-a8d369c0',
     'RHEL-7.1': 'ami-12663b7a',
     'SUSE-SLES-12': 'ami-aeb532c6',
-    'Ubuntu-12.04': 'ami-dcfaa6b4',
-    'Ubuntu-14.04': 'ami-6889d200',
+    'Ubuntu-14.04': 'ami-767a391e',
     'DTR': 'ami-75df221e',
 }
 
 US_WEST_1 = {
+    'AMAZON-LINUX': 'ami-87ea13c3',
+    'CENTOS-7.0': 'ami-6bcfc42e',
+    'DEBIAN-JESSIE': 'ami-a5d621e1',
+    'DEBIAN-WHEEZY': 'ami-b4869ff1',
+    'RHEL-7.0': 'ami-33cdd876',
+    'RHEL-7.1': 'ami-a540a5e1',
+    'SUSE-SLES-12': 'ami-b95b4ffc',
+    'Ubuntu-14.04': 'ami-8ebba3cb',
+    'DTR': 'ami-998d48dd',
 }
 
 US_WEST_2 = {
+    'AMAZON-LINUX': 'ami-d5c5d1e5',
+    'CENTOS-7.0': 'ami-c7d092f7',
+    'DEBIAN-JESSIE': 'ami-3d56520d',
+    'DEBIAN-WHEEZY': 'ami-431a4273',
+    'RHEL-7.0': 'ami-99bef1a9',
+    'RHEL-7.1': 'ami-4dbf9e7d',
+    'SUSE-SLES-12': 'ami-d7450be7',
+    'Ubuntu-14.04': 'ami-1b471c2b',
+    'DTR': 'ami-d51b07e5',
 }
 
 AP_SOUTHEAST_2 = {
+    'AMAZON-LINUX': 'ami-db7b39e1',
+    'CENTOS-7.0': 'ami-bd523087',
+    'DEBIAN-JESSIE': 'ami-07e3993d',
+    'DEBIAN-WHEEZY': 'ami-6b770351',
+    'RHEL-7.0': 'ami-83e08db9',
+    'RHEL-7.1': 'ami-d3daace9',
+    'SUSE-SLES-12': 'ami-b90e6283',
+    'Ubuntu-14.04': 'ami-e7eb9fdd',
 }
 
 AP_SOUTHEAST_1 = {
+    'AMAZON-LINUX': 'ami-d44b4286',
+    'CENTOS-7.0': 'ami-aea582fc',
+    'DEBIAN-JESSIE': 'ami-60989c32',
+    'DEBIAN-WHEEZY': 'ami-aeb49ffc',
+    'RHEL-7.0': 'ami-941031c6',
+    'RHEL-7.1': 'ami-dc1c2b8e',
+    'SUSE-SLES-12': 'ami-84b392d6',
+    'Ubuntu-14.04': 'ami-36c6ec64',
 }
