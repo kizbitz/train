a9dd5d9e2135dda0aaa6f4b4df3a05852ae23c0d
kizbitz <jerry.baker@docker.com>
2016-01-23 08:52:20 -0500

    Update UCP lab
    Signed-off-by: kizbitz <jerry.baker@docker.com>

--- train/labs/ucp/description.md
+++ train/labs/ucp/description.md
@@ -2,4 +2,8 @@ Name: ucp
 
 Description:
 
-Launches 3 instances and pulls the latest UCP images
+Launches 3 AWS instances:
+
+- node-0: Pull latest UCP images and prompt to do a non-interctive install
+- node-1: Pull latest UCP images
+- node-2: Pull latest UCP images
--- train/labs/ucp/scripts/ubuntu-14.04.py
+++ train/labs/ucp/scripts/ubuntu-14.04.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import getpass
+from distutils.util import strtobool
 
-def prompt_pass(question):
-    pass1 = getpass.getpass(question)
-    pass2 = getpass.getpass("Confirm password: ")
+def yn_prompt(query):
+    """Generic Y/N Prompt"""
 
-    if pass1 != pass2:
-        prompt_pass(question)
-    else:
-        return pass1
+    sys.stdout.write('%s [y/n]: ' % query)
+    val = raw_input()
+    try:
+        ret = strtobool(val)
+    except ValueError:
+        sys.stdout.write('Please answer with a y/n\n')
+        return yn_prompt(query)
+    return ret
 
 
 # prompts
-ubuntu_pass = prompt_pass("Enter 'ubuntu' password for nodes: ")
-hub_name = raw_input("Enter your Docker Hub username: ")
-hub_pass = prompt_pass("Enter your Docker Hub password: ")
-hub_email = raw_input("Enter your Docker Hub email: ")
+install = yn_prompt("\nInstall UCP using 'non-interactive' mode on the controller instance?")
+
+if install:
+    txt = 'docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install'
+else:
+    txt = 'docker run --name ucp --rm -v /var/run/docker.sock:/var/run/docker.sock docker/ucp images'
 
 
 # scripts
 PRIMARY_OS = 'Ubuntu-14.04'
-PRIMARY = '''#!/bin/sh
+CONTROLLER = '''#!/bin/sh
 
 FQDN="{{fqdn}}"
 
@@ -36,16 +41,6 @@ sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
 echo $FQDN > /etc/hostname
 service hostname restart
 sleep 5
-
-# packages
-apt-get update
-apt-get -y upgrade
-apt-get install -y git tree jq xfsprogs linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
-
-# password authentication
-echo ubuntu:{0} | chpasswd
-sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
-service ssh restart
 
 # docker cs release
 wget -qO- 'https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import
@@ -61,21 +56,55 @@ usermod -aG docker ubuntu
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# pull the ucp images
-docker login --username="{1}" --password="{2}" --email="{3}"
+# packages
+apt-get -y upgrade
+apt-get install -y git tree jq xfsprogs linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
 
-docker pull dockerorca/ucp-controller
-docker pull dockerorca/ucp
-docker pull dockerorca/ucp-cfssl
-docker pull dockerorca/ucp-cfssl-proxy
-docker pull dockerorca/ucp-proxy
-docker pull swarm
-docker pull dockerorca/ucp-etcd
-docker pull dockerorca/dsinfo
+# download/install upc
+{0}
 
 {{dinfo}}
-reboot
-'''.format(ubuntu_pass, hub_name, hub_pass, hub_email)
+'''.format(txt)
+
+
+NODE = '''#!/bin/sh
+
+FQDN="{fqdn}"
+
+export DEBIAN_FRONTEND=noninteractive
+
+# locale
+sudo locale-gen en_US.UTF-8
+
+# /etc/hostname - /etc/hosts
+sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
+echo $FQDN > /etc/hostname
+service hostname restart
+sleep 5
+
+# docker cs release
+wget -qO- 'https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import
+apt-get update
+apt-get install -y apt-transport-https
+echo "deb https://packages.docker.com/1.9/apt/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
+
+usermod -aG docker ubuntu
+
+# docker compose
+curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# packages
+apt-get -y upgrade
+apt-get install -y git tree jq xfsprogs linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
+
+# download upc
+docker run --name ucp --rm -v /var/run/docker.sock:/var/run/docker.sock docker/ucp images
+
+{dinfo}
+'''
 
 
 AMIBUILD = '''#!/bin/sh
@@ -88,14 +117,9 @@ echo $FQDN > /etc/hostname
 service hostname restart
 sleep 5
 
-# password authentication
-echo ubuntu:{0} | chpasswd
-sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
-service ssh restart
-
 {{dinfo}}
 reboot
-'''.format(ubuntu_pass)
+'''
 
 
 def pre_process():
