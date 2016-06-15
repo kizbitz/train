20cfc130d278871200235ae54e798e5bf2952110
kizbitz <jerry.baker@docker.com>
2016-06-15 08:01:00 -0400

    Set zones on workshops. Fix ddc workshop
    Signed-off-by: kizbitz <jerry.baker@docker.com>

--- train/labs/hols-1.12/scripts/ubuntu-14.04.py
+++ train/labs/hols-1.12/scripts/ubuntu-14.04.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # prompts
+gh_name = raw_input('Enter your Github username: ')
+gh_pass = raw_input('Enter your Github password: ')
 
 # instance configs
 PRIMARY_OS = 'Ubuntu-14.04'
@@ -22,6 +24,8 @@ sleep 5
 
 # docker os release
 curl -sSL https://experimental.docker.com/ | sh
+#curl -O https://github.com/docker/docker-1.12-integration/releases/download/TP6/docker-1.12.0-dev.tgz /tmp/docker.tgz
+#tar -xvzf /tmp/docker.tgz --strip=1 -C /usr/bin/
 
 # updates
 apt-get update
@@ -31,22 +35,6 @@ apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.
 # compose
 curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# output cleanup script
-cat >/usr/local/bin/docker-reset <<EOL
-#!/bin/bash
-sudo service docker stop
-# /etc/default/docker
-sudo rm /etc/default/docker
-sudo cp /etc/default/docker.bak /etc/default/docker
-unset -v DOCKER_HOST
-unset -v DOCKER_OPTS
-unset -v DOCKER_CONTENT_TRUST
-sudo rm -r /var/lib/docker
-sudo service docker start
-EOL
-
-chmod +x /usr/local/bin/docker-reset
 
 service docker stop
 rm -r /var/lib/docker
--- train/labs/workshop-ddc/scripts/ubuntu-14.04.py
+++ train/labs/workshop-ddc/scripts/ubuntu-14.04.py
@@ -4,14 +4,11 @@
 import sys
 from distutils.util import strtobool
 
-# Preinstall UCP for the Dockercon training classes
-txt = 'docker run --rm --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --host-address $(curl http://169.254.169.254/latest/meta-data/public-ipv4)'
-
 # scripts
 PRIMARY_OS = 'Ubuntu-14.04'
 CONTROLLER = '''#!/bin/sh
 
-FQDN="{{fqdn}}"
+FQDN="{fqdn}"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -24,27 +21,14 @@ echo $FQDN > /etc/hostname
 service hostname restart
 sleep 5
 
-# docker
-curl -sSL https://get.docker.com/ | sh
-
-usermod -aG docker ubuntu
-
 # updates
 apt-get update
 apt-get -y upgrade
 apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
 
-# docker compose
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# download/install upc
-{0}
-
-{{dinfo}}
-
+{dinfo}
 reboot
-'''.format(txt)
+'''
 
 
 NODE = '''#!/bin/sh
@@ -67,20 +51,7 @@ apt-get update
 apt-get -y upgrade
 apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
 
-# docker
-curl -sSL https://get.docker.com/ | sh
-
-usermod -aG docker ubuntu
-
-# docker compose
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# download upc
-docker run --name ucp --rm -v /var/run/docker.sock:/var/run/docker.sock docker/ucp images
-
 {dinfo}
-
 reboot
 '''
 
@@ -99,32 +70,12 @@ echo $FQDN > /etc/hostname
 service hostname restart
 sleep 5
 
-# updates/docker cs engine
-wget -qO- 'https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import
-echo "deb https://packages.docker.com/1.10/apt/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+# updates
 apt-get update
 apt-get -y upgrade
-apt-get install -y \
-    apt-transport-https \
-    docker-engine \
-    git \
-    tree \
-    jq \
-    linux-image-extra-4.2.0-23-generic \
-    linux-image-4.2.0.23-generic \
-    linux-image-extra-virtual
-
-usermod -aG docker ubuntu
-
-# docker compose
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# dtr
-bash -c "$(sudo docker run docker/trusted-registry install)"
+apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
 
 {dinfo}
-
 reboot
 '''
 
