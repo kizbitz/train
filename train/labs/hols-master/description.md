1a0663b793e980d0859942caf4f41e263e8b6803
kizbitz <jerry.baker@docker.com>
2016-06-17 09:54:34 -0400

    Add hols-master lab
    Signed-off-by: kizbitz <jerry.baker@docker.com>

---
+++ train/labs/hols-master/description.md
@@ -0,0 +1,19 @@
+Name: hols-master
+
+Description:
+
+6 Ubuntu 14.04 nodes:
+
+-  v111node0
+-  v111node1
+-  v111node2
+-  v112node0
+-  v112node1
+-  v112node2
+
+Packages:
+  - git
+  - tree
+  - jq
+  - linux-image-extra-4.2.0-23-generic
+  - linux-image-4.2.0.23-generic
---
+++ train/labs/hols-master/scripts/ubuntu-14.04.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# prompts
+
+# instance configs
+PRIMARY_OS = 'Ubuntu-14.04'
+V111 = '''#!/bin/sh
+
+FQDN="{fqdn}"
+
+export DEBIAN_FRONTEND=noninteractive
+
+# locale
+sudo locale-gen en_US.UTF-8
+
+# /etc/hostname - /etc/hosts
+sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
+echo $FQDN > /etc/hostname
+service hostname restart
+sleep 5
+
+# docker os release
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker ubuntu
+
+# updates
+apt-get update
+apt-get -y upgrade
+apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
+
+# compose
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+{dinfo}
+reboot
+'''
+
+V112 = '''#!/bin/sh
+
+FQDN="{fqdn}"
+
+export DEBIAN_FRONTEND=noninteractive
+
+# locale
+sudo locale-gen en_US.UTF-8
+
+# /etc/hostname - /etc/hosts
+sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
+echo $FQDN > /etc/hostname
+service hostname restart
+sleep 5
+
+# docker rc release
+curl -fsSL https://test.docker.com/ | sh
+usermod -aG docker ubuntu
+
+# updates
+apt-get update
+apt-get -y upgrade
+apt-get install -y git tree jq linux-image-extra-4.2.0-23-generic linux-image-4.2.0.23-generic
+
+# compose
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+{dinfo}
+reboot
+'''
+
+# Script to use if launching from a custom lab AMI image
+AMIBUILD = '''#!/bin/sh
+
+FQDN="{fqdn}"
+
+# /etc/hostname - /etc/hosts
+sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
+echo $FQDN > /etc/hostname
+service hostname restart
+sleep 5
+
+{dinfo}
+'''
+
+
+def pre_process():
+    """Executed before launching instances in AWS"""
+    pass
+
+def post_process():
+    """Executed after launching instances in AWS"""
+    pass
