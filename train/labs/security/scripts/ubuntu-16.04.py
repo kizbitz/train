379a99a6f71889f1609227d7ae0957174a832039
kizbitz <jerry.baker@docker.com>
2017-02-15 13:38:41 -0500

    Update 'security' lab
    Signed-off-by: kizbitz <jerry.baker@docker.com>

--- train/labs/security/scripts/ubuntu-16.04.py
+++ train/labs/security/scripts/ubuntu-16.04.py
@@ -1,42 +1,53 @@
 #!/usr/bin/env python
 
 # scripts
-UBUNTU16 = 'Ubuntu-16.04'
-DEFAULT16 = '''#!/bin/sh
+PRIMARY_OS = 'Ubuntu-16.04'
+PRIMARY = '''#!/bin/sh
 
-FQDN="{fqdn}"
+FQDN="{{fqdn}}"
+
+export DEBIAN_FRONTEND=noninteractive
+
+# locale
+locale-gen en_US.UTF-8
 
 # hostname
 hostnamectl set-hostname $FQDN
 sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
 
 # packages
-apt-get update
-apt-get install -y htop git tree jq seccomp strace
+curl -s 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | apt-key add --import
+echo "deb https://packages.docker.com/1.13/apt/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update && apt-get install -y \
+    apt-transport-https \
+    docker-engine \
+    git \
+    htop \
+    jq \
+    inux-image-extra-$(uname -r) \
+    linux-image-extra-virtual \
+    seccomp \
+    strace \
+    tree
 
-# docker
-apt-get install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y docker-engine=1.12.0-0~xenial
 usermod -aG docker ubuntu
 
 # compose
 curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-{dinfo}
+{{dinfo}}
 reboot
-'''
+'''.format()
 
 # Script to use if launching from a custom lab AMI image
 AMIBUILD = '''#!/bin/sh
-#
+
 FQDN="{{fqdn}}"
 
 # hostname
 hostnamectl set-hostname $FQDN
+sed -i "1 c\\127.0.0.1 $FQDN localhost" /etc/hosts
 
 {{dinfo}}
 reboot
